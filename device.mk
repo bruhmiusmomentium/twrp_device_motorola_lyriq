fdeb6bbaec9b39b0edef46655ddeeab25cfde95b
CloudedQuartz <ayan.pande06@gmail.com>
2023-06-04 00:50:42 +0530

    lyriq: use shared library instead of PRODUCT_STATIC_BOOT_CONTROL_HAL
    device/motorola/lyriq/device.mk:24: error: PRODUCT_STATIC_BOOT_CONTROL_HAL is obsolete. Use shared library module instead. See https://android.googlesource.com/platform/build/+/master/Changes.md#PRODUCT_STATIC_BOOT_CONTROL_HAL.
    
    Signed-off-by: CloudedQuartz <ayan.pande06@gmail.com>

--- device.mk
+++ device.mk
@@ -16,16 +16,12 @@ AB_OTA_POSTINSTALL_CONFIG += \
 # Boot control HAL
 PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-impl \
+    android.hardware.boot@1.0-impl.recovery \
     android.hardware.boot@1.0-service
 
 PRODUCT_PACKAGES += \
-    bootctrl.mt6893
-
-PRODUCT_STATIC_BOOT_CONTROL_HAL := \
     bootctrl.mt6893 \
-    libgptutils \
-    libz \
-    libcutils
+    bootctrl.mt6893.recovery
 
 PRODUCT_PACKAGES += \
     otapreopt_script \
